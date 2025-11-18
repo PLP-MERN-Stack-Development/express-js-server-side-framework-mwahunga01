c56f313e64c47fbf14bcc7d28070a3d3d8aa92a8
mwahunga01 <mwahungabrian@gmail.com>
2025-11-18 09:02:59 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,10 @@
+const auth = (req, res, next) => {
+  const apiKey = req.header('x-api-key');
 
+  if (!apiKey || apiKey !== process.env.API_KEY) {
+    return res.status(401).json({ error: 'Unauthorized – Invalid or missing API key' });
+  }
+  next();
+};
+
+module.exports = auth;
